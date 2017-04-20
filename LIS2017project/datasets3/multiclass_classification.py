2647bec8be2002b2296fcd3f8c40550380d71c83
chenqian <hittjcq7356131>
2017-04-20 13:23:32 +0200

    extra-tree classifier

--- LIS2017project/datasets3/multiclass_classification.py
+++ LIS2017project/datasets3/multiclass_classification.py
@@ -52,24 +52,31 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.linear_model import SGDClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import PolynomialFeatures
+
 
 def accu_score(y_true,y_pred):
     acc=accuracy_score(y_true,y_pred)
     return acc
 
-Train_data=pd.read_csv('train.csv').values
-X_train=Train_data[:,2:]
-y_train=Train_data[:,1]
+rawDataTrain = pd.read_csv('train.csv').values
+X_train = rawDataTrain[:, 2:]   # the features
+y_train = rawDataTrain[:, 1]   # the target value
+X_train = PolynomialFeatures(degree=3).fit_transform(X_train)
 
-Test_data=pd.read_csv('test.csv').values
-X_test=Test_data[:,1:]
-y_test=np.zeros([X_test.shape[0],2])
-y_test[:,0]=Test_data[:,0]
+rawDataTest = pd.read_csv('test.csv').values
+X_test = rawDataTest[:, 1:]
+X_test = PolynomialFeatures(degree=3).fit_transform(X_test)
+y_test = np.zeros([X_test.shape[0], 2])   # [id, y]
+y_test[:, 0] = rawDataTest[:, 0]
 
-clf=DecisionTreeClassifier(random_state=0)
+# clf=DecisionTreeClassifier(random_state=0)
 # clf=KNeighborsClassifier(n_neighbors=3)
 # clf=SGDClassifier(loss='hinge')
 # clf=MLPClassifier(solver='lbfgs', alpha=1e-5)
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier(n_estimators=300, criterion='entropy', max_features=None,
+                                   bootstrap=True, min_samples_leaf=1, max_leaf_nodes=None, class_weight='balanced_subsample')
 clf.fit(X_train,y_train)
 # y_pred=clf.predict(X_train)
 # acc=accuracy_score(y_train,y_pred)
