3b6a2b59103c4ef1bb54b72034dfe6c3078463ec
chenqian <hittjcq7356131>
2017-04-25 11:19:54 +0200

    calculate derivatives (math.diff is not applicable?)

--- LIS2017project/datasets4/task3_0420.py
+++ LIS2017project/datasets4/task3_0420.py
@@ -1,31 +1,14 @@
-from mpl_toolkits.axes_grid.inset_locator import inset_axes
-import matplotlib.pyplot as plt
-import numpy as np
+#tips: use "administrator" to install keras, tensorflow, theano in anaconda prompt and cmd simutaneously
 
-dt=0.001
-t=np.arange(0.0,10.0,dt)
-r=np.exp(-t[:1000]/0.05)
-x=np.random.rand(len(t))
-s=np.convolve(x,r)[:len(x)]*dt
+import math
+def f(x):
+    return math.tanh(x)
 
-fig=plt.figure(figsize=(9,4),facecolor='white')
-ax=fig.add_subplot(121)
-plt.plot(t,s)
-plt.axis([0,1,1.1*np.amin(s),2*np.amax(s)])
-plt.xlabel('time(s)')
-plt.ylabel('current(nA)')
-plt.title('Subplot1')
+def deriviate(x):
+    h=1/10000
+    rise=f(x+h)-f(x)
+    run=h
+    slope=rise/run
+    return slope
 
-inset_axes=inset_axes(ax, width='50%',height=1.0,loc=1)
-n,bins,patches=plt.hist(s,400,normed=1)
-plt.xticks([])
-plt.yticks([])
-ax=fig.add_subplot(122)
-plt.plot(t,s)
-plt.axis([0, 1, 1.1*np.amin(s), 2*np.amax(s)])
-plt.xlabel('time (s)')
-plt.ylabel('current (nA)')
-plt.title('Subplot 2: \n Gaussian colored noise')
-
-plt.tight_layout()
-plt.show()
+print(deriviate(-0.2024))
