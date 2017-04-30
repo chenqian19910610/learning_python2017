98433521266dd239f27f3888414fbd922598b8bb
chenqian <hittjcq7356131>
2017-04-30 16:40:06 +0200

    format in python

--- python beginner 2016fall/random.py
+++ python beginner 2016fall/random.py
@@ -1,35 +1,50 @@
-""""
-from scipy import stats
-import numpy as np
-x = np.random.random(10)
-y = np.random.random(10)
-slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
-
-
-import pandas as pd
-import numpy as np
-df= df.dropna(axis=1, how="") #delete the column, axis=0 is delete the rows
-df["types"]=df["types"].astype("category") #overwrite the original string column into category type
-df.isnull.any()
-
-
- function.attribute
- function(argument)
-
-
-
-import pandas as pd
-import numpy as np
-pip install -U scikit-learn
 """
+format in python
+"""
+a='{1} {2} {0}'.format('one','two','three')
+print(a)
 
-import numpy as np
-from scipy.integrate import quad
-def integrand (t,n,x):
-    return np.exp(-x*t)/t**n
-def expint(n,x):
-    return quad(integrand, 1, np.inf, args=(n,x))[0]
-vec_expint=np.vectorize (expint)
-vec_expint(3,np.arange (1,4,0.5))
+a='{:>10}'.format('test') # align to the right
+a='{:^10}'.format('test') # align to the middle
+print(a)
+
+a='{:.3}'.format('abcdefghijk') # truncating words to specified length
+a='{:>10.3}'.format('abcdefghijk') # combine truncating and padding
+print(a)
+
+numbers=[23.00,0.123545684,1,4.2,9887.3] #floats
+for number in numbers:
+    print('{:10.4f}'.format(number))
+
+a=round(12358546546, -5) # round integers
+print(a)
+
+a='{:+d}'.format(42) # print signs
+b='{:d}'.format(-42)
+print(a,b)
+
+data={'first':'abc','second':'123'} #named placeholders
+print('{first}{second}'.format(**data))
+
+person={'firstname':'Qian','lastname':'Chen'} # how to get information !!!
+print('{p[firstname]} {p[lastname]}'.format(p=person))
+data=[4,5,6,7,8]
+print('{d[0]} {d[3]}'.format(d=data))
+
+class Plant(object):
+    type='tree'
+    kinds=[{'name':'oak'},{'name':'maple'}]
+a='{p.type}:{p.kinds[0][name]}'.format(p=Plant()) #按照给定的格式输出指定的值
+print(a)
+
+from datetime import datetime #print time
+a='{:%Y-%m-%d %H:%M}'.format(datetime(2017,4,30,16,25))
+print(a)
+
+a='{:{prec}}={:{prec}}'.format('chenqian',2.7184568,prec='.3')#truncate with parameterized format
+print(a)
+"""
+numpy in python
+"""
 
 
