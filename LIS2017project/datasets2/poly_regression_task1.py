855bfc9452b70ac7278bbd855be54de2e9b108b0
chenqian <hittjcq7356131>
2017-05-12 17:50:57 +0200

    plot PCA x1,x2

--- LIS2017project/datasets2/poly_regression_task1.py
+++ LIS2017project/datasets2/poly_regression_task1.py
@@ -4,46 +4,81 @@ Packages: sklearn: http://scikit-learn.org/stable/modules/classes.html#module-sk
 LassoCV: http://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.PolynomialFeatures.html#examples-using-sklearn-preprocessing-polynomialfeatures
 Polinomial features:http://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.PolynomialFeatures.html#examples-using-sklearn-preprocessing-polynomialfeatures
 """
-import pandas as pd   # for import and export the data to csv
-from sklearn.metrics import mean_squared_error as mse
-from sklearn.metrics import make_scorer
-from sklearn.model_selection import cross_val_score
-import numpy as np
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LassoCV
+# import pandas as pd   # for import and export the data to csv
+# from sklearn.metrics import mean_squared_error as mse
+# from sklearn.metrics import make_scorer
+# from sklearn.model_selection import cross_val_score
+# import numpy as np
+# from sklearn.preprocessing import PolynomialFeatures
+# from sklearn.linear_model import LassoCV
+#
+#
+# def root_mean_squared_error(y_true, y_pred, sample_weight=None, multioutput='uniform_average'):
+#     rmse = np.sqrt(mse(y_true, y_pred,
+#                        sample_weight=sample_weight, multioutput=multioutput))
+#     return rmse
+#
+# # import data
+# # for training
+# rawDataTrain = pd.read_csv('train.csv').values
+# X_train = rawDataTrain[:, 2:]   # the features
+# y_train = rawDataTrain[:, 1]   # the target value
+# X_train = PolynomialFeatures(degree=3).fit_transform(X_train)
+#
+#
+# # for test
+# rawDataTest = pd.read_csv('test.csv').values
+# X_test = rawDataTest[:, 1:]
+# X_test = PolynomialFeatures(degree=3).fit_transform(X_test)
+# y_test = np.zeros([X_test.shape[0], 2])   # [id, y]
+# y_test[:, 0] = rawDataTest[:, 0]
+#
+# # Lassocv
+# clf = LassoCV(cv=10)
+# clf.fit(X_train, y_train)
+#
+# cvScore = cross_val_score(clf,
+#                           X_train, y_train,
+#                           scoring=make_scorer(root_mean_squared_error),
+#                           verbose=0,
+#                           cv=10)
+# print('Mean of CV RMSE:', np.mean(cvScore))
+#
+# y_test[:, 1] = clf.predict(X_test)
+# df = pd.DataFrame(data=y_test)
+# df.to_csv('./lassocv.csv', header=['Id', 'y'], sep=',', index=False)
 
-
-def root_mean_squared_error(y_true, y_pred, sample_weight=None, multioutput='uniform_average'):
-    rmse = np.sqrt(mse(y_true, y_pred,
-                       sample_weight=sample_weight, multioutput=multioutput))
-    return rmse
-
-# import data
-# for training
-rawDataTrain = pd.read_csv('train.csv').values
-X_train = rawDataTrain[:, 2:]   # the features
-y_train = rawDataTrain[:, 1]   # the target value
-X_train = PolynomialFeatures(degree=3).fit_transform(X_train)
-
-
-# for test
-rawDataTest = pd.read_csv('test.csv').values
-X_test = rawDataTest[:, 1:]
-X_test = PolynomialFeatures(degree=3).fit_transform(X_test)
-y_test = np.zeros([X_test.shape[0], 2])   # [id, y]
-y_test[:, 0] = rawDataTest[:, 0]
-
-# Lassocv
-clf = LassoCV(cv=10)
-clf.fit(X_train, y_train)
-
-cvScore = cross_val_score(clf,
-                          X_train, y_train,
-                          scoring=make_scorer(root_mean_squared_error),
-                          verbose=0,
-                          cv=10)
-print('Mean of CV RMSE:', np.mean(cvScore))
-
-y_test[:, 1] = clf.predict(X_test)
-df = pd.DataFrame(data=y_test)
-df.to_csv('./lassocv.csv', header=['Id', 'y'], sep=',', index=False)
+"""
+visualize PCA-2Dimension
+"""
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import pandas as pd
+# from sklearn.decomposition import PCA
+# from sklearn.svm import SVR
+#
+# rawDataTrain = pd.read_csv('train.csv').values
+# X_train = rawDataTrain[:, 2:]
+# y_train = rawDataTrain[:, 1]
+# X_train_1 = PCA(n_components=2).fit(X_train).transform(X_train)
+# print(X_train_1.shape)
+#
+# clf=SVR(kernel='rbf',C=1)
+# clf.fit(X_train_1,y_train)
+#
+# h=0.02
+# x1_min,x1_max=X_train_1[:,0].min()-1,X_train_1[:,0].max()+1
+# x2_min,x2_max=X_train_1[:,1].min()-1,X_train_1[:,1].max()+1
+# xx,yy=np.meshgrid(np.arange(x1_min,x1_max,h),np.arange(x2_min,x2_max,h))
+# Z=clf.predict(np.c_[xx.ravel(),yy.ravel()])
+# Z=Z.reshape(xx.shape)
+#
+# plt.contourf(xx,yy,Z,alpha=0.3)
+# plt.title('PCA-SVC')
+# plt.xlim(x1_min,x1_max)
+# plt.ylim(x2_min,x2_max)
+# plt.xlabel('x1')
+# plt.ylabel('x2')
+# plt.scatter(X_train_1[:,0],X_train_1[:,1],c=y_train,alpha=0.8)
+#
+# plt.show()
--- LIS2017project/datasets5/task-4.py
+++ LIS2017project/datasets5/task-4.py
@@ -1,44 +1,44 @@
 """
 MLP-train labeled data
 """
-# from keras.models import Sequential
-# from keras.layers import Dense,Dropout,Activation
-# from keras.optimizers import SGD
-# from keras import regularizers
-# import numpy as np
-# import pandas as pd
-#
-# train_labeled=pd.read_hdf('train_labeled.h5','train').values
-# train_unlabeled=pd.read_hdf('train_unlabeled.h5','train').values
-# test_data=pd.read_hdf('test.h5','test').values
-#
-# X_trainlab=train_labeled[:,1:]
-# y_trainlab=train_labeled[:,0]
-# X_trainun=train_unlabeled[:,:]
-# X_test=test_data[:,:]
-# y_test=np.zeros([X_test.shape[0],2])
-# y_test[:,0]=pd.read_csv('sample.csv', index_col=False).values[:,0]
-#
-# from keras.utils.np_utils import to_categorical
-# y_trainlab=to_categorical(y_trainlab)
-#
-# model=Sequential()
-# model.add(Dense(256,activation='relu',input_dim=128))
-# model.add(Dropout(0.5))
-# model.add(Dense(256,activation='relu'))
-# model.add(Dropout(0.25))
-# model.add(Dense(128,activation='relu'))
-# model.add(Dropout(0.25))
-# model.add(Dense(10,activation='softmax'))
-# model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['categorical_accuracy'])
-#
-# model.fit(X_trainlab,y_trainlab,epochs=500,batch_size=128)
-# score=model.evaluate(X_trainlab,y_trainlab,batch_size=128)
-# print(score)
-#
-# y_test[:,1]=model.predict_classes(X_test)
-# df=pd.DataFrame(data=y_test)
-# df.to_csv('MLP-trainlabeled.csv', header=['Id', 'y'], sep=',', index=False, float_format='%.0f')
+from keras.models import Sequential
+from keras.layers import Dense,Dropout,Activation
+from keras.optimizers import SGD
+from keras import regularizers
+import numpy as np
+import pandas as pd
+
+train_labeled=pd.read_hdf('train_labeled.h5','train').values
+train_unlabeled=pd.read_hdf('train_unlabeled.h5','train').values
+test_data=pd.read_hdf('test.h5','test').values
+
+X_trainlab=train_labeled[:,1:]
+y_trainlab=train_labeled[:,0]
+X_trainun=train_unlabeled[:,:]
+X_test=test_data[:,:]
+y_test=np.zeros([X_test.shape[0],2])
+y_test[:,0]=pd.read_csv('sample.csv', index_col=False).values[:,0]
+
+from keras.utils.np_utils import to_categorical
+y_trainlab=to_categorical(y_trainlab)
+
+model=Sequential()
+model.add(Dense(256,activation='relu',input_dim=128))
+model.add(Dropout(0.5))
+model.add(Dense(256,activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(128,activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10,activation='softmax'))
+model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['categorical_accuracy'])
+
+model.fit(X_trainlab,y_trainlab,epochs=500,batch_size=128)
+score=model.evaluate(X_trainlab,y_trainlab,batch_size=128)
+print(score)
+
+y_test[:,1]=model.predict_classes(X_test)
+df=pd.DataFrame(data=y_test)
+df.to_csv('MLP-trainlabeled.csv', header=['Id', 'y'], sep=',', index=False, float_format='%.0f')
 
 """
 cluster-unlabel-low accuracy
